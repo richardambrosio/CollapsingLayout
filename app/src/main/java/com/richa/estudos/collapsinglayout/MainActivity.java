5e2c629b8698611c2b79b362eb1711a3698d748c
Richard Ambrosio <richard.ambrosio1@gmail.com>
2019-04-23 11:07:08 -0300

    Initial commit

---
+++ app/src/main/java/com/richa/estudos/collapsinglayout/MainActivity.java
@@ -0,0 +1,28 @@
+package com.richa.estudos.collapsinglayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Adicionando o botão de voltar na toolbar
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
